2b4be9977254def8e2d87495cd231bc20b0184e3
Pete Hunt <phunt@twitter.com>
2021-04-13 12:53:19 -0700

    Clean up the benchmark output and rerun

--- README.md
+++ README.md
@@ -7,23 +7,31 @@ It is stupid, but it is also an apples-to-apples comparison, and indicates the o
 When I run it on my 2020 13" MBP I get the following output:
 
 ```
-Cold compile: java
+Wrote 50 files per language, 1000 statements each
 
-real	0m1.663s
-user	0m6.373s
-sys	0m0.207s
+java
 
-Cold compile: esbuild (no typechecking)
+real	0m1.907s
+user	0m6.657s
+sys	0m0.239s
 
-real	0m0.054s
-user	0m0.196s
-sys	0m0.071s
+golang
 
-Cold compile: tsc (typechecking)
+real	0m0.247s
+user	0m0.226s
+sys	0m0.233s
 
-real	0m3.529s
-user	0m4.825s
-sys	0m0.170s
+TypeScript, no type checking (esbuild)
+
+real	0m0.058s
+user	0m0.204s
+sys	0m0.075s
+
+TypeScript, with type checking (tsc)
+
+real	0m3.789s
+user	0m5.201s
+sys	0m0.199s
 ```
 
 tsc with typechecking is about 2x slower than javac. esbuild is about 30x faster than javac.
--- generate_test_files.py
+++ generate_test_files.py
@@ -1,8 +1,11 @@
+num_statements = 1000
+num_files = 50
+
 def generate_java(name):
     lines = []
     lines.append('class %s {' % name)
     lines.append('  public static void main(String[] args) {')
-    for i in range(1000):
+    for i in range(num_statements):
         lines.append('    System.out.println("Hello world %d");' % i)
     lines.append('  }')
     lines.append('}')
@@ -10,7 +13,7 @@ def generate_java(name):
 
 def generate_ts():
     lines = []
-    for i in range(1000):
+    for i in range(num_statements):
         lines.append('console.log("Hello world %d");' % i)
     return '\n'.join(lines)
 
@@ -19,12 +22,12 @@ def generate_golang(name):
     lines.append('package main')
     lines.append('import "fmt"')
     lines.append('func %s() {' % name)
-    for i in range(1000):
+    for i in range(num_statements):
         lines.append('    fmt.Println("Hello world %d")' % i)
     lines.append('}')
     return '\n'.join(lines)
 
-for i in range(50):
+for i in range(num_files):
     with open('ts/hello%d.ts' % i, 'w') as f:
         f.write(generate_ts())
     with open('java/Hello%d.java' % i, 'w') as f:
@@ -35,3 +38,5 @@ for i in range(50):
         else:
             name = 'hello%d' % i
         f.write(generate_golang(name))
+
+print('Wrote %d files per language, %d statements each' % (num_files, num_statements))
